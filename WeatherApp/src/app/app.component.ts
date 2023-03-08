f70a4cd0c798114050ca938505a655d6a233cdea
StefanovicL <stefanovic560@gmail.com>
2023-03-08 14:21:18 +0100

    first commit

---
+++ WeatherApp/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Day, WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  cityName: string = 'Belgrade';
+  weatherData?: WeatherData;
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+
+        console.log(response);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+}
